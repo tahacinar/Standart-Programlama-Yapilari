7cbba04b4d5cab118dc7a577136500ece0cf3485
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-01 23:34:12 +0300

    1.0.

---
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/33-DongulerİsimTekrarla.kt
@@ -0,0 +1,21 @@
+package com.tahacinar.standartprogramlamayapilari
+
+import java.util.*
+
+fun main() {
+
+    val girdi = Scanner(System.`in`)
+
+    println("İsim giriniz")
+
+    val isim = girdi.next()
+
+    println("Tekrar sayısını giriniz")
+
+    val tekrar = girdi.nextInt()
+
+    for (i in 1..tekrar) {
+        println("$i. $isim")
+    }
+
+}
