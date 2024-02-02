2c4c38040947bcd4c5c8899668ec4881db3319a5
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-03 00:08:30 +0300

    1.0.

--- app/src/main/java/com/tahacinar/standartprogramlamayapilari/37-DiziCalismasi.kt
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/37-DiziCalismasi.kt
@@ -16,5 +16,52 @@ fun main(){
 
     val meyveler = arrayOf<String>("Çilek","Muz","Elma","Kivi","Kiraz")
 
+    //Verilere erişim
+
+    val str1 = meyveler[2]
+
+    println(str1)
+
+    val str2 = meyveler.get(3)
+
+    println(str2)
+
+    //Veri üzerinde işlem yapma
+
+    meyveler[1] = "Yeni Muz"
+
+    println(meyveler.contentToString())
+
+    meyveler.set(2,"Yeni Elma")
+
+    println(meyveler.contentToString())
+
+    //Array işlemleri
+
+    println(meyveler.isEmpty())
+
+    println(meyveler.count())
+
+    println(meyveler.first())
+
+    println(meyveler.last())
+
+    println(meyveler.indexOf("Kivi"))
+
+    println(meyveler.contains("Yeni Muz"))
+
+    println(meyveler.max())
+
+    println(meyveler.min())
+
+    println(meyveler.sort())
+
+    println(meyveler.contentToString())
+
+    println(meyveler.reverse())
+
+    println(meyveler.contentToString())
+
+
 
 }
