d5a7f1a0a438960931aa987deb022cebd09b49ee
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-03 00:08:03 +0300

    1.0.

---
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/37-DiziCalismasi.kt
@@ -0,0 +1,20 @@
+package com.tahacinar.standartprogramlamayapilari
+
+fun main(){
+
+    //Tanımlama
+
+    val dizi1 = Array<Int>(5){0}//[0,0,0,0,0]
+
+    val dizi2 = arrayOf(1,10,7)
+
+    val dizi3 = arrayOf<Int>(1,36,9)
+
+    val dizi4 = arrayOf<String>("Çilek","Ahmet")
+
+    val dizi5 = arrayOf(3,8,"Elma","Bursa")
+
+    val meyveler = arrayOf<String>("Çilek","Muz","Elma","Kivi","Kiraz")
+
+
+}
