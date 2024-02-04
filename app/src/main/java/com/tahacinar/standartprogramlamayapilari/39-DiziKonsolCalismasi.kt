65a907e7a78124f370bf8a902f44cd750d3d9023
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-04 17:02:42 +0300

    1.0.

--- app/src/main/java/com/tahacinar/standartprogramlamayapilari/39-DiziKonsolCalismasi.kt
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/39-DiziKonsolCalismasi.kt
@@ -1,2 +1,11 @@
 package com.tahacinar.standartprogramlamayapilari
 
+import java.util.Scanner
+
+fun main() {
+
+    val girdi = Scanner(System.`in`)
+
+    val isimler = Array<String>(5) { "" }//["","Yeni Kayıt","","",""]
+//0,1,2,3,4
+}
