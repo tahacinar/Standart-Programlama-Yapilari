7be1f9a8747fb73e82e8130c17a306c50966e325
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-05 21:21:37 +0300

    1.0.

--- app/src/main/java/com/tahacinar/standartprogramlamayapilari/40-DiziKarneUygulamasi.kt
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/40-DiziKarneUygulamasi.kt
@@ -1,2 +1,12 @@
 package com.tahacinar.standartprogramlamayapilari
 
+import java.util.Scanner
+
+fun main(){
+
+    val dersler = Array<String>(5){""}//["tarih","fizik","","",""]
+    val notlar = Array<Int>(5){0}//[40,60,4,0,0]
+
+    val girdi = Scanner(System.`in`)
+
+}
