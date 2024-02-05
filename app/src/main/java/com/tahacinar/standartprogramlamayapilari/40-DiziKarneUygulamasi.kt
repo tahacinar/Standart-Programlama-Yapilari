9fe359ad2e229f8dbf6d6e9f7aaa80814cccd0e5
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-02-05 21:22:01 +0300

    1.0.

--- app/src/main/java/com/tahacinar/standartprogramlamayapilari/40-DiziKarneUygulamasi.kt
+++ app/src/main/java/com/tahacinar/standartprogramlamayapilari/40-DiziKarneUygulamasi.kt
@@ -9,4 +9,14 @@ fun main(){
 
     val girdi = Scanner(System.`in`)
 
+    for ( i in 0 until dersler.count() ) {//0,1,2,3,4
+        println("${i+1}. dersi giriniz")
+        val ders = girdi.next()
+        dersler[i] = ders
+
+        println("${i+1}. notu giriniz")
+        val not = girdi.nextInt()
+        notlar[i] = not
+    }
+
 }
